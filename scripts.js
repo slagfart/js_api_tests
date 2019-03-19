8f657b3320f7383695c6b55068795a92b1980fd7
Dimitri Vasdekis <dimitrivasdekis@Dimitris-MacBook-Pro.local>
2019-03-20 10:37:58 +1100

    Following Tutorial

--- scripts.js
+++ scripts.js
@@ -1,26 +1,12 @@
-//Create a new request variable and assign this inbuilt XHR object slash method to it
-var my_req = new XMLHttpRequest()
+//get the contents of the root element in the html file
+const app = document.getElementById('root')
 
-//Open a new connection, using the GET request on the URL endpoint
-my_req.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+//Create an image element on the site
+const logo = document.createElement('img')
+logo.src = 'logo.png'
 
-//Do something when the data from the API call returns, but not before (onload function)
-my_req.onload = function () {
-	
-	//Begin accessing JSON here
-	var my_data = JSON.parse(this.response)
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
 
-
-	//check if errors version
-	if (my_req.status >= 200 && my_req.status < 400) {
-		my_data.forEach(movie => {
-			console.log(movie.title)
-		})
-	} else {
-		console.log('error')
-	}
-}
-
-
-//Send the built request
-my_req.send()
+app.appendChild(logo)
+app.appendChild(container)
